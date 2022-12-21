202bbeafaf629930c9c81d14a05696ddb7a650ba
ChamakeNjefi <chamakenjefi@gmail.com>
2022-12-21 01:30:52 +0000

    Providers

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
-provider "azurerm" {
-  features {}
-}
-
-provider "aws" {
-  region = "us-east-1"
+resource "null_resource" "mk" {
+  provisioner "local-exec" {
+    command = "echo '0' > status.txt"
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo '1' > status.txt"
+  }
 }

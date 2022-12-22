88afd285a3e1f4c00ae8a8398c213aac1095aa1f
ChamakeNjefi <chamakenjefi@gmail.com>
2022-12-22 17:06:58 +0000

    updated

--- aws-s3-remote-storage/variables.tf
+++ aws-s3-remote-storage/variables.tf
@@ -2,7 +2,7 @@ variable "external_port" {
   type    = number
   default = 8081
   validation {
-    condition     = can(regex("8080|80", var.external_port))
+    condition     = can(regex("8081|80", var.external_port))
     error_message = "Port values can only be 8081 or 80."
   }
 }

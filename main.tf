180d5f2b5d23dbf5ede45abf0ebf973b43273431
ChamakeNjefi <chamakenjefi@gmail.com>
2023-01-06 01:11:06 +0000

    updated file

--- main.tf
+++ main.tf
@@ -9,59 +9,64 @@ terraform {
 
 provider "docker" {}
 
+variable "ext_port" {
+    type    = number
+    default = 1880
+    
+    validation {
+      condition = var.ext_port <= 65535 && var.ext_port > 0 
+      error_message = "The external port must be in the valid port range."
+      }
+}
+
+variable "int_port" {
+    type    = number
+    default = 1880
+
+    validation {
+      condition = var.int_port == 1880
+      error_message = "The internal port must be 1880."
+      }
+}
+
+variable "container_count" {
+    type    = number
+    default = 1
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "random_string" "random" {
+    count = var.container_count
     length = 4
     special = false
     upper = false 
 }
 
-resource "random_string" "random2" {
-    length = 4
-    special = false
-    upper = false
-}
-
-resource "docker_container" "nodered_container" {
-  name  = join("-", ["nodered", random_string.random.result])
+ resource "docker_container" "nodered_container" {
+  count = var.container_count
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
-    internal = 1880
-    # external = 1880
-  }
-}
-
-resource "docker_container" "nodered_container2" {
-  name  = join("-", ["nodered2", random_string.random2.result])
-  image = docker_image.nodered_image.latest
-  ports {
-    internal = 1880
-    # external = 1880
+    internal = var.int_port
+    external = var.ext_port
   }
 }
 
 output "IP-Address" {
-  value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
-  description = "ip address and external port of the container"
+ value       = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address], i.ports[*]["external"])]
+ description = "ip address and external port of the container"
 }
 
 output "container-name" {
-  value       = docker_container.nodered_container.name
-  description = "name of the container"
+ value       = docker_container.nodered_container[*].name
+ description = "name of the container"
 
 }
 
-output "IP-Address2" {
-  value       = join(":", [docker_container.nodered_container2.ip_address, docker_container.nodered_container2.ports[0].external])
-  description = "ip address and external port of the container"
-}
 
-output "container-name2" {
-  value       = docker_container.nodered_container2.name
-  description = "name of the container"
 
-}
+
 
